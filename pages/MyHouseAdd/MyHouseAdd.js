71d8827ddcb3a5eab4b3fc576a0e258b7d8383fc
syb <1208874645@qq.com>
2019-02-11 18:10:20 +0800

    完善三级联动

--- pages/MyHouseAdd/MyHouseAdd.js
+++ pages/MyHouseAdd/MyHouseAdd.js
@@ -6,120 +6,116 @@ Page({
    */
   data: {
     region: ['广东省', '广州市', '海珠区'],
+    communityData: [{
+      "label": "万佳御园",
+      "value": "6",
+      "children": null
+    }, {
+        "label": "天隆佳园",
+        "value": "7",
+        "children": null
+      }],
+    countryAndTownData: [{
+      "label": "伊旗",
+      "value": "4",
+      "children": [
+        {
+          "label": "上湾",
+          "value": "17",
+          "children": null
+        },
+        {
+          "label": "阿镇",
+          "value": "5",
+          "children": null
+        }
+      ]
+    },
+      {
+        "label": "乌审旗",
+        "value": "22",
+        "children": null
+      }],
     provinceAndCityData: [{ "label": "内蒙古", "value": "2", "children": [{ "label": "鄂尔多斯", "value": "3", "children": null }, { "label": "包头", "value": "10", "children": null }] }, { "label": "陕西", "value": "23", "children": [{"label":"榆林市","value":"24","children":null},{"label":"西安市","value":"27","children":null}] }],
-    customItem: '全部',
-    multiIndex: [0, 0],
-    multiArray: []
-    // multiArray:[
-    //   {
-    //     citys: [
-    //       {
-    //         areas: [
-    //           {
-    //             code: "440303",
-    //             name: "罗湖区"
-    //           },
-    //           {
-    //             code: "440304",
-    //             name: "福田区"
-    //           }
-    //                      ],
-    //         code: "440300",
-    //         name: "深圳市"
-    //       }
-    //         ],
-    //     code: "440000",
-    //     name: "广东省"
-    //   },
-    //   {
-    //     name:"内蒙古自治区",
-    //     code: "150000",
-    //     citys:[
-    //       {
-    //         name:"鄂尔多斯市",
-    //         code:"152700",
-    //         areas:[
-    //           {
-    //             name: "伊金霍洛旗",
-    //             code: "152728"
-    //           },
-    //           {
-    //             name: "东胜",
-    //             code:"152701"
-    //           }
-    //         ]
-    //       }
-    //     ]
-    //   }
-    // ]
-  },
-
-  getCityByProvinceIndex: function (indexOfProvince) {
-    let originalData = this.data.provinceAndCityData;
-    let city = originalData[indexOfProvince].children;
-    return city;
-  },
-
-  //点击确定
-  bindMultiPickerChange: function (e) {
-    console.log('picker发送选择改变，携带值为', e.detail.value)
-    this.setData({
-      multiIndex: e.detail.value
-    })
-  },
-  //滑动
-  bindMultiPickerColumnChange: function (e) {
-    console.log('修改的列为', e.detail.column, '，值为', e.detail.value);
-    var data = {
-      multiArray: this.data.multiArray,
-      multiIndex: this.data.multiIndex
-    };
-    
-    if(e.detail.column == 0){
-      let label = this.data.provinceAndCityData[e.detail.value].label;
-      console.log(label);
-      let city = this.getCityByProvinceIndex(e.detail.value);
-      console.log('city'); console.log(city);
-      data.multiArray[1] = city;
-      console.log('multiArray'); console.log(data.multiArray);
-      this.setData(data);
-      this.setData({
-        multiIndex:[e.detail.value,0]
-      });
-    }
-
-
-    //setData更新数据
-    //this.setData(data);
+    roomData: [
+      {
+        "label": "5号楼",
+        "value": "7",
+        "children": [
+          {
+            "label": "1单元",
+            "value": "8",
+            "children": [
+              {
+                "label": "903室",
+                "value": "9",
+                "children": null
+              },
+              {
+                "label": "103室",
+                "value": "26",
+                "children": null
+              }
+            ]
+          },
+          {
+            "label": "1单元",
+            "value": "25",
+            "children": null
+          }
+        ]
+      },
+      {
+        "label": "7号楼",
+        "value": "24",
+        "children": [
+          {
+            "label": "1单元",
+            "value": "8",
+            "children": [
+              {
+                "label": "112室",
+                "value": "9",
+                "children": null
+              },
+              {
+                "label": "113室",
+                "value": "26",
+                "children": null
+              }
+            ]
+          },
+          {
+            "label": "2单元",
+            "value": "25",
+            "children": [
+              {
+                "label": "222室",
+                "value": "9",
+                "children": null
+              },
+              {
+                "label": "223室",
+                "value": "26",
+                "children": null
+              }
+            ]
+          }
+        ]
+      }
+    ]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
-    let originalData = this.data.provinceAndCityData;
-    let multiArray = [];
-    let province = [];
-    // let city = [];
-    for(let i=0;i<originalData.length;i++){
-      let temp = {
-        label:originalData[i].label,
-        value:originalData[i].value
-      }
-      province.push(temp);
-    }
 
-    let indexOfProvince = this.data.multiIndex[0];
-    let indexOfCity = this.data.multiIndex[1];
+  },
 
-    let city = originalData[indexOfProvince].children;
-    multiArray.push(province);
-    multiArray.push(city);
-
-    this.setData({
-      multiArray: multiArray
-    })
+  onPickerChoosed: function(e){
+    console.log("接收到数据：");
+    console.log(e.detail);
   },
 
   /**
--- utils/components/CustomeredPicker/CustomeredPicker.js
+++ utils/components/CustomeredPicker/CustomeredPicker.js
@@ -4,71 +4,159 @@ Component({
    * 组件的属性列表
    */
   properties: {
+    name: String,
     text: String,
     cols: Number,
-    region: Array
+    dataArray: Array
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-    multiIndex: [0, 0],
-    multiArray: []
+    multiIndex: [0,0],
+    multiArray: [],
+    content: ''
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-    getCityByProvinceIndex: function (indexOfProvince) {
-      let originalData = this.data.region;
-      let city = originalData[indexOfProvince].children;
-      return city;
+    //只有2个层级时，修改了第一层级，第二层级的内容随着改变
+    getSecondByFirstIndex: function (indexOfFirst) {
+      let originalData = this.data.dataArray;
+      let second = originalData[indexOfFirst].children;
+      if(second == null || second == undefined) second = [];
+      return second;
     },
 
+    //有3个层级时，修改了第一层级时，第二层级的内容随着改变，第三层级默认是0
+    getThirdByFirstIndex: function(indexOfFirst){
+      let originalData = this.data.dataArray;
+      let second = originalData[indexOfFirst].children;
+      if (second == null || second == undefined) second = [];
+      let third = second[0] == undefined?[]:second[0].children;
+      if (third == null || third == undefined) third = [];
+      return third;
+    },
+    //有3个层级，修改了第二层级，第三层级的内容随着改变
+    getThirdBySecondAndFirstIndex: function(indexOfFirst,indexOfSecond){
+      let originalData = this.data.dataArray;
+      let second = originalData[indexOfFirst].children;
+
+      let third = second[indexOfSecond] == undefined?[]:second[indexOfSecond].children;
+      if (third == null || third == undefined) third = [];
+      return third;
+    },
+
+    //选中了一个选项
+    bindMultiPickerChange(e){
+      this.setData({
+        multiIndex: e.detail.value
+      });
+      let originalData = this.data.dataArray;
+      let cols = this.data.cols;
+      let id = [];
+      id.push(originalData[e.detail.value[0]].value);
+      let content = originalData[e.detail.value[0]].label;
+      if(cols == 2) {
+        id.push(originalData[e.detail.value[0]].children[e.detail.value[1]].value);
+        content = content + "," + originalData[e.detail.value[0]].children[e.detail.value[1]].label;
+      }
+      if (cols == 3) {
+        id.push(originalData[e.detail.value[0]].children[e.detail.value[1]].value);
+        id.push(originalData[e.detail.value[0]].children[e.detail.value[1]].children[e.detail.value[2]].value);
+        content = content + "," + originalData[e.detail.value[0]].children[e.detail.value[1]].label +"," + originalData[e.detail.value[0]].children[e.detail.value[1]].children[e.detail.value[2]].label;
+      }
+      this.setData({content:content});
+      let myEventData = {
+        name: this.data.name,
+        value: id
+      }
+      this.triggerEvent('onPickerChoosed',myEventData);
+    },
+
+    //滑动改变了picker
     bindMultiPickerColumnChange(e){
-      console.log('修改的列为', e.detail.column, '，值为', e.detail.value);
+      let cols = this.data.cols;
       var data = {
         multiArray: this.data.multiArray,
         multiIndex: this.data.multiIndex
       };
 
       if (e.detail.column == 0) {
-        let label = this.data.region[e.detail.value].label;
-        console.log(label);
-        let city = this.getCityByProvinceIndex(e.detail.value);
-        console.log('city'); console.log(city);
-        data.multiArray[1] = city;
-        console.log('multiArray'); console.log(data.multiArray);
+        let second = null; if (cols >= 2) second = this.getSecondByFirstIndex(e.detail.value);
+        let third = null; if (cols >= 3) third = this.getThirdByFirstIndex(e.detail.value);
+        
+        if(cols >= 2) data.multiArray[1] = second;
+        if(cols >= 3) data.multiArray[2] = third;
+
+        let multiIndex = [];
+        if(cols >= 1) multiIndex.push(e.detail.value);
+        if(cols >= 2) multiIndex.push(0);
+        if(cols >= 3) multiIndex.push(0);
+        data.multiIndex = multiIndex;
+
         this.setData(data);
-        this.setData({
-          multiIndex: [e.detail.value, 0]
-        });
+      }
+      else if(e.detail.column == 1){
+        if(cols >= 3) {
+          let indexOfFirst = this.data.multiIndex[0];
+          let indexOfSecond = e.detail.value;
+
+          let third = this.getThirdBySecondAndFirstIndex(indexOfFirst,indexOfSecond);
+          data.multiArray[2] = third;
+          data.multiIndex = [indexOfFirst,indexOfSecond,0];
+          this.setData(data);
+        }
       }
     }
   },
 
   lifetimes:{
+    //组件挂载时初始化data
     attached(){
-      let originalData = this.data.region;
+      let originalData = this.data.dataArray;
+      let cols = this.data.cols;
       let multiArray = [];
-      let province = [];
-      // let city = [];
+      let indexOfFirst = this.data.multiIndex[0];
+      let indexOfSecond = this.data.multiIndex[1];
+
+      let first = [];
       for (let i = 0; i < originalData.length; i++) {
         let temp = {
           label: originalData[i].label,
           value: originalData[i].value
         }
-        province.push(temp);
+        first.push(temp);
       }
 
-      let indexOfProvince = this.data.multiIndex[0];
-      let indexOfCity = this.data.multiIndex[1];
+      let second = [];
+      let secondOriginalData = originalData[indexOfFirst].children;
+      if (secondOriginalData == null || secondOriginalData == undefined) secondOriginalData = [];
+      for (let i = 0; i < secondOriginalData.length; i++) {
+        let temp = {
+          label: originalData[indexOfFirst].children[i].label,
+          value: originalData[indexOfFirst].children[i].value
+        }
+        second.push(temp);
+      }
 
-      let city = originalData[indexOfProvince].children;
-      multiArray.push(province);
-      multiArray.push(city);
+      let third = [];
+      let thirdOriginalData = secondOriginalData[indexOfSecond] == undefined?[]:secondOriginalData[indexOfSecond].children;
+      if (thirdOriginalData == null || thirdOriginalData == undefined) thirdOriginalData = [];
+      for (let i = 0; i < thirdOriginalData.length; i++) {
+        let temp = {
+          label: secondOriginalData[indexOfSecond].children[i].label,
+          value: secondOriginalData[indexOfSecond].children[i].value
+        }
+        third.push(temp);
+      }
+
+      if(cols >= 1) multiArray.push(first);
+      if(cols >= 2) multiArray.push(second);
+      if(cols == 3) multiArray.push(third);
 
       this.setData({
         multiArray: multiArray
